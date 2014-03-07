da61ec89c53e2ed793ad08a765241e7220127189
Michael Zhilin <zhilin@netcracker.com>
2014-03-07 13:51:28 +0400

    (fix) compilation error of Main after CLI improvement

--- kSar/src/net/atomique/ksar/Main.java
+++ kSar/src/net/atomique/ksar/Main.java
@@ -20,14 +20,15 @@ import net.atomique.ksar.UI.Desktop;
 import net.atomique.ksar.UI.SplashScreen;
 
 /**
- *
+ * 
  * @author Max
  */
 public class Main {
 
     static Config config = null;
     static GlobalOptions globaloptions = null;
-    static ResourceBundle resource = ResourceBundle.getBundle("net/atomique/ksar/Language/Message");
+    static ResourceBundle resource = ResourceBundle
+            .getBundle("net/atomique/ksar/Language/Message");
 
     ;
 
@@ -36,22 +37,28 @@ public class Main {
     }
 
     public static void show_version() {
-        System.err.println("ksar Version : " + VersionNumber.getVersionNumber());
+        System.err
+                .println("ksar Version : " + VersionNumber.getVersionNumber());
     }
 
     private static void set_lookandfeel() {
-        for (UIManager.LookAndFeelInfo laf : UIManager.getInstalledLookAndFeels()) {
+        for (UIManager.LookAndFeelInfo laf : UIManager
+                .getInstalledLookAndFeels()) {
             if (Config.getLandf().equals(laf.getName())) {
                 try {
                     UIManager.setLookAndFeel(laf.getClassName());
                 } catch (ClassNotFoundException ex) {
-                    Logger.getLogger(Desktop.class.getName()).log(Level.SEVERE, null, ex);
+                    Logger.getLogger(Desktop.class.getName()).log(Level.SEVERE,
+                            null, ex);
                 } catch (InstantiationException ex) {
-                    Logger.getLogger(Desktop.class.getName()).log(Level.SEVERE, null, ex);
+                    Logger.getLogger(Desktop.class.getName()).log(Level.SEVERE,
+                            null, ex);
                 } catch (IllegalAccessException ex) {
-                    Logger.getLogger(Desktop.class.getName()).log(Level.SEVERE, null, ex);
+                    Logger.getLogger(Desktop.class.getName()).log(Level.SEVERE,
+                            null, ex);
                 } catch (UnsupportedLookAndFeelException ex) {
-                    Logger.getLogger(Desktop.class.getName()).log(Level.SEVERE, null, ex);
+                    Logger.getLogger(Desktop.class.getName()).log(Level.SEVERE,
+                            null, ex);
                 }
             }
         }
@@ -67,7 +74,6 @@ public class Main {
             }
         }
 
-
         set_lookandfeel();
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
 
@@ -80,120 +86,122 @@ public class Main {
         });
 
     }
-		int i = 0;
-		String arg;
-		// / load default
-		String mrjVersion = System.getProperty("mrj.version");
-		if (mrjVersion != null) {
-			System.setProperty("com.apple.mrj.application.growbox.intrudes",
-					"false");
-			System.setProperty(
-					"com.apple.mrj.application.apple.menu.about.name", "kSar");
-			System.setProperty("apple.laf.useScreenMenuBar", "true");
-		}
 
-		config = Config.getInstance();
-		globaloptions = GlobalOptions.getInstance();
+    public static void main(String[] args) {
+        int i = 0;
+        String arg;
+        // / load default
+        String mrjVersion = System.getProperty("mrj.version");
+        if (mrjVersion != null) {
+            System.setProperty("com.apple.mrj.application.growbox.intrudes",
+                    "false");
+            System.setProperty(
+                    "com.apple.mrj.application.apple.menu.about.name", "kSar");
+            System.setProperty("apple.laf.useScreenMenuBar", "true");
+        }
 
-		if (args.length > 0) {
-			while (i < args.length && args[i].startsWith("-")) {
-				arg = args[i++];
-				if ("-version".equals(arg)) {
-					show_version();
-					System.exit(0);
-				}
-				if ("-help".equals(arg)) {
-					usage();
-					continue;
-				}
-				if ("-test".equals(arg)) {
-					GlobalOptions.setDodebug(true);
-					continue;
-				}
-				if ("-input".equals(arg)) {
-					if (i < args.length) {
-						GlobalOptions.setCLfilename(args[i++]);
-					} else {
-						exit_error(resource.getString("INPUT_REQUIRE_ARG"));
-					}
-					continue;
-				}
-				if ("-graph".equals(arg)) {
-					if (i < args.length) {
-						StringTokenizer t = new StringTokenizer(args[i++], ",");
-						while (t.hasMoreTokens()) {
-							GlobalOptions.getGraphList().add(t.nextToken());
-						}
-					} else {
-						exit_error("-graph requires a graph list");
-					}
-				}
+        config = Config.getInstance();
+        globaloptions = GlobalOptions.getInstance();
 
-				if ("-outputPNG".equals(arg)) {
-					if (i < args.length) {
-						GlobalOptions.setOutputPNGBase(args[(i++)]);
-					} else {
-						exit_error("-outputPNG requires a base filename");
-					}
-				}
-			}
-		}
-		System.out.println("MIZH");
-		if (GlobalOptions.getOutputPNGBase() == null)
-			make_ui();
-		else {
-			System.setProperty("java.awt.headless", "true");
-			kSar mysar = new kSar();
-			mysar.do_fileread(GlobalOptions.getCLfilename());
+        if (args.length > 0) {
+            while (i < args.length && args[i].startsWith("-")) {
+                arg = args[i++];
+                if ("-version".equals(arg)) {
+                    show_version();
+                    System.exit(0);
+                }
+                if ("-help".equals(arg)) {
+                    usage();
+                    continue;
+                }
+                if ("-test".equals(arg)) {
+                    GlobalOptions.setDodebug(true);
+                    continue;
+                }
+                if ("-input".equals(arg)) {
+                    if (i < args.length) {
+                        GlobalOptions.setCLfilename(args[i++]);
+                    } else {
+                        exit_error(resource.getString("INPUT_REQUIRE_ARG"));
+                    }
+                    continue;
+                }
+                if ("-graph".equals(arg)) {
+                    if (i < args.length) {
+                        StringTokenizer t = new StringTokenizer(args[i++], ",");
+                        while (t.hasMoreTokens()) {
+                            GlobalOptions.getGraphList().add(t.nextToken());
+                        }
+                    } else {
+                        exit_error("-graph requires a graph list");
+                    }
+                }
 
-			try {
-				mysar.launched_action.join();
-			} catch (Exception e) {
-			}
+                if ("-outputPNG".equals(arg)) {
+                    if (i < args.length) {
+                        GlobalOptions.setOutputPNGBase(args[(i++)]);
+                    } else {
+                        exit_error("-outputPNG requires a base filename");
+                    }
+                }
+            }
+        }
+        System.out.println("MIZH");
+        if (GlobalOptions.getOutputPNGBase() == null)
+            make_ui();
+        else {
+            System.setProperty("java.awt.headless", "true");
+            kSar mysar = new kSar();
+            mysar.do_fileread(GlobalOptions.getCLfilename());
 
-			System.out.println(((Linux) mysar.myparser).ListofGraph);
-			for (Map.Entry<String, Object> entry : ((Linux) mysar.myparser).ListofGraph
-					.entrySet()) {
-				if (entry.getValue() instanceof Graph) {
-					if (GlobalOptions.getGraphList().size() > 0
-							&& !GlobalOptions.getGraphList().contains(
-									entry.getKey()))
-						continue;
-					System.out.println("GRAPH");
-					((Graph) entry.getValue()).savePNG(
-							null,
-							null,
-							GlobalOptions.getOutputPNGBase() + "_"
-									+ entry.getKey() + ".png", 800, 600);
-				}
-				if (entry.getValue() instanceof List) {
-					System.out.println("LIST");
-					System.out.println(((List) entry.getValue()).nodeHashList);
-					for (Map.Entry<String, Graph> ent : ((List) entry
-							.getValue()).nodeHashList.entrySet()) {
-						if (GlobalOptions.getGraphList().size() > 0
-								&& !GlobalOptions.getGraphList().contains(
-										entry.getKey())
-								&& !GlobalOptions.getGraphList().contains(
-										entry.getKey() + "_" + ent.getKey()))
-							continue;
-						ent.getValue().savePNG(
-								null,
-								null,
-								GlobalOptions.getOutputPNGBase() + "_"
-										+ entry.getKey() + "_" + ent.getKey()
-										+ ".png", 800, 400);
-					}
-				}
-			}
-		}
+            try {
+                mysar.launched_action.join();
+            } catch (Exception e) {
+            }
 
-		System.out.println("exit");
-	}
+            System.out.println(((Linux) mysar.myparser).ListofGraph);
+            for (Map.Entry<String, Object> entry : ((Linux) mysar.myparser).ListofGraph
+                    .entrySet()) {
+                if (entry.getValue() instanceof Graph) {
+                    if (GlobalOptions.getGraphList().size() > 0
+                            && !GlobalOptions.getGraphList().contains(
+                                    entry.getKey()))
+                        continue;
+                    System.out.println("GRAPH");
+                    ((Graph) entry.getValue()).savePNG(
+                            null,
+                            null,
+                            GlobalOptions.getOutputPNGBase() + "_"
+                                    + entry.getKey() + ".png", 800, 600);
+                }
+                if (entry.getValue() instanceof List) {
+                    System.out.println("LIST");
+                    System.out.println(((List) entry.getValue()).nodeHashList);
+                    for (Map.Entry<String, Graph> ent : ((List) entry
+                            .getValue()).nodeHashList.entrySet()) {
+                        if (GlobalOptions.getGraphList().size() > 0
+                                && !GlobalOptions.getGraphList().contains(
+                                        entry.getKey())
+                                && !GlobalOptions.getGraphList().contains(
+                                        entry.getKey() + "_" + ent.getKey()))
+                            continue;
+                        ent.getValue().savePNG(
+                                null,
+                                null,
+                                GlobalOptions.getOutputPNGBase() + "_"
+                                        + entry.getKey() + "_" + ent.getKey()
+                                        + ".png", 800, 400);
+                    }
+                }
+            }
+        }
 
-	public static void exit_error(final String message) {
-		System.err.println(message);
-		System.exit(1);
+        System.out.println("exit");
+    }
 
-	}
+    public static void exit_error(final String message) {
+        System.err.println(message);
+        System.exit(1);
+
+    }
 }
